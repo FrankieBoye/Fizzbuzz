8b4541438b2660d1a595ac1db8b34e84342a80f1
Frankie <frankie@frankie.com>
2019-03-25 21:31:21 +0000

    slight refactoring

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,13 +1,11 @@
 def fizzbuzz(number)
   if number % 15 == 0   
   "Fizzbuzz"
-  else
-  if number % 5 == 0
+  elsif number % 5 == 0
   "Buzz" 
   elsif number % 3 == 0
   "Fizz"
   else
     number  
   end
-  end  
 end
