ae1b6227c0095ce44ba2ceeab40c16c9f8729a80
Frankie <frankie@frankie.com>
2019-03-25 14:57:36 +0000

    fizzbuzz test 3

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,7 +1,11 @@
 def fizzbuzz(number)
-  if number > 3    
-  "fizz"
+  if number % 3 == 0   
+  "Fizz"
+  else
+  if number % 5 == 0
+  "Buzz" 
   else
     number  
   end
-end  
+  end  
+end
--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -1,6 +1,13 @@
 require 'fizzbuzz'
 describe 'fizzbuzz' do
- it 'returns "fizz" when passed 3' do
-  expect(fizzbuzz(3)).to eq 'fizz'
+ it 'returns "Fizz" when passed the number is divisble by 3' do
+  expect(fizzbuzz(3)).to eq 'Fizz'
  end
 end 
+
+describe 'fizzbuzz' do
+ it 'returns "Buzz" when passed the number is divisble by 5' do
+  expect(fizzbuzz(5)).to eq 'Buzz'
+ end
+end 
+
